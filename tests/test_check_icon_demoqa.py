f1a298008c2b168f2f74e77271595b2280bcd866
Egor_Kurguzkin <egor-kurg@yandex.ru>
2025-03-20 14:56:32 +0300

    Second commit

--- tests/test_check_icon_demoqa.py
+++ tests/test_check_icon_demoqa.py
@@ -1,11 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
+from pages.demoqa import DemoQa
 
-driver = webdriver.Chrome()
-driver.get("https://demoqa.com/")
+def test_check_icon(browser):
+    # browser.get("https://demoqa.com/")
+    #
+    # icon = browser.find_element(By.CSS_SELECTOR, '#app > header > a')
+    #
+    # if icon is None:
+    #     print('Элемент не найден')
+    # else:
+    #     print('Элемент найден')
 
-icon = driver.find_element(By.CSS_SELECTOR, 'header > a > img')
-if icon is None:
-    print('Элемент не найден')
-else:
-    print('Элемент найден')
+    demo_qa_page = DemoQa(browser)
+    demo_qa_page.visit()
+    demo_qa_page.click_on_the_icon()
+    assert demo_qa_page.equal_url()
+    assert demo_qa_page.exist_icon()
